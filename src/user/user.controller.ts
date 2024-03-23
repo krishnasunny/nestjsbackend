ba865fbfada62fb6d05a5550693d178d074ec648
krishnasunny <sunnyfriend2012@gmail.com>
2024-03-23 11:36:51 +0530

    Change in user controller
    added post request in the user controller
    
    Co-Authored-By: vahid-nejad <vahid884@gmail.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards,Post,Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
@@ -9,6 +9,13 @@ export class UserController {
   @UseGuards(JwtGuard)
   @Get(':id')
   async getUserProfile(@Param('id') id: number) {
+    console.log(id);
     return await this.userService.findById(id);
+  }
+
+  @Post(':id')
+  async updateUserProfile(@Body() dto: any) {
+    console.log(dto);
+    // return await this.userService.findById(dto);
   }
 }
